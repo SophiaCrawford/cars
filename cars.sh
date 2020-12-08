fabfd2f1be9c48ae30216937454bfb2c0fe8f843
Sophia Crawford <socrawford@chapman.edu>
2020-12-07 23:40:01 -0800

    Adds cars.sh

--- cars.sh
+++ cars.sh
@@ -4,16 +4,18 @@
 while
 	true
 do
-	echo -n "Type the number 1 to enter a new car
-        Type the number 2 to display the list of cars
-        Type the number 3 to quit and exit the program"; read number
+	echo "Type the number 1 to enter a new car"
+        echo "Type the number 2 to display the list of cars"
+        echo -n "Type the number 3 to quit and exit the program"; read -r number
 	case "$number" in
-		"1") echo "Enter the car year: "; read year;;
-	             echo "Enter the car make: "; read make;;
-                     echo "Enter the car model: "; read model;;
-                     echo "$year:$make:$model" >> My_old_cars     
-                "2") sort My_old_cars
+		"1") echo "Enter the car year: "; read -r year
+	             echo "Enter the car make: "; read -r make
+                     echo "Enter the car model: "; read -r model
+                     echo "$year:$make:$model" >> My_old_cars;;     
+                "2") sort My_old_cars;;
                 "3") echo "Goodbye!"
+		     break;;
+	esac
 done
 # *) echo "ERROR - Please enter a number (1,2,3): "; read number
 
